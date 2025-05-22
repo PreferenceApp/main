db59ec9ae11f0283abf9762767be5519691ecbf0
PreferenceApp <preferenceapp@gmail.com>
2025-05-22 09:48:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ export default async ({ req, res, log, error }) => {
   if (req.path === "/") 
   { 
     const event = req.headers['x-appwrite-event'];
-    if(event === "users." + req.body.userId + ".sessions." + req.body.$id + ".create")
+    if(event === "users." + userId + ".create")
+    {
+      try
+      {
+        const createLastMessageSentDoc = await db.createDocument('db', 'lastMessageSent', userId, {discordUsername: userId }, [ Permission.read(Role.user(userId)) ]);
+      }
+      catch(err)
+      {
+        error(err);
+      }
+    }
+    else if(event === "users." + req.body.userId + ".sessions." + req.body.$id + ".create")
     {
       let userData = null;
       try {
@@ -58,138 +69,52 @@ export default async ({ req, res, log, error }) => {
       }
     }
   }
-  else if(req.path === "/totals")
-  {
-      const listAllDocs = await db.listDocuments('db', 'users', [
-        Query.equal('userB', [user.name]),
-        Query.limit(5000)
-      ]);
-
-      if(user.labels.length > 0)
-      {
-        const unixTime = Math.floor(Date.now() / 1000);
-        log(unixTime);
-          if(parseInt(user.labels[0]) >= unixTime)
-          {
-              return res.json({ status: listAllDocs.documents });
-          }
-          else
-          {
-            return res.json({ status: listAllDocs.total });
-          }
-      }
-      else
-      {
-          return res.json({ status: listAllDocs.total });
-      }
-     // log(listAllDocs);
-  }
   else if(req.path === "/like")
   {
-      const body = JSON.parse(req.body);
-      const userB = body.userB;
-      if(!userB)
-      {
-         const getMyUserDoc = await db.getDocument('db', 'users', userId);
-
-        try 
-        {
-          if(getMyUserDoc.match)
-          {
-              const listTheirDoc = await db.listDocuments('db', 'users', 
-              [
-                Query.equal('userA', [getMyUserDoc.userB]),
-              ]);
-
-              if(listTheirDoc.documents.length > 0)
-              {
-                const getTheirUserDoc = listTheirDoc.documents[0];
-                const updateTheirDoc = await db.updateDocument('db', 'users', getTheirUserDoc.$id, { userA: getTheirUserDoc.userA, userB: getTheirUserDoc.userB, match: false }, [ Permission.read(Role.user(getTheirUserDoc.$id)) ]);
-              }
-  
-          }
-        }
-        catch (err)
-        {
-            error('Error unmatching with user:', err);
-        }
-        finally
-        {
-            const updateUserDoc = await db.updateDocument('db', 'users', userId, { userA: getMyUserDoc.userA, userB: null, match: false }, [ Permission.read(Role.user(userId)) ]);
-        
-        }
-        
-        return res.json({ status: `Successfully unliked everyone` });
-      }
-
-      const listTheirDoc = await db.listDocuments('db', 'users', 
-      [
-        Query.equal('userA', [userB]),
-      ]);
-
-      if(listTheirDoc.documents.length === 0)
-      {
-        return res.json({ status: `${userB} isn't registered.` });
-      }
-
-      const getMyUserDoc = await db.getDocument('db', 'users', userId);
-      const getTheirUserDoc = listTheirDoc.documents[0];
-
-      if(getMyUserDoc.match)
-      {
-          return res.json({ status: `You can't like ${userB} this week because you've already matched with ${getMyUserDoc.userB}. Try again next week!` });
-      }
-
-      if(getTheirUserDoc.match)
-      {
-          return res.json({ status: `You can't like ${userB} this week because they've already matched with someone else. Try again next week!` });
-      }
-    
-      if(getTheirUserDoc.userB)
-      {
-          if(getTheirUserDoc.userB === getMyUserDoc.userA)
-          {
-              //It's a match!
-              const updateUserDoc = await db.updateDocument('db', 'users', userId, { userA: getMyUserDoc.userA, userB: userB, match: true }, [ Permission.read(Role.user(userId)) ]);
-              const updateTheirDoc = await db.updateDocument('db', 'users', getTheirUserDoc.$id, { userA: getTheirUserDoc.userA, userB: getTheirUserDoc.userB, match: true }, [ Permission.read(Role.user(getTheirUserDoc.$id)) ]);
-          }
-          else
-          {
-              const updateUserDoc = await db.updateDocument('db', 'users', userId, { userA: getMyUserDoc.userA, userB: userB, match: false }, [ Permission.read(Role.user(userId)) ]);
-          }
-      }
-      else
-      {
-          const updateUserDoc = await db.updateDocument('db', 'users', userId, { userA: getMyUserDoc.userA, userB: userB, match: false }, [ Permission.read(Role.user(userId)) ]);
-      }
+    //can't be done client side
+  }
+  else if(req.path === "/unlike")
+  {
+    //can't be done client side
+  }
+  else if(req.path === "/crushes")
+  {
+    //can't be done client side
   }
   else if(req.path === "/delete")
   {
       try {
-        const getMyUserDoc = await db.getDocument('db', 'users', userId);
-        if(getMyUserDoc.match)
-        {
-            const listTheirDoc = await db.listDocuments('db', 'users', 
-            [
-              Query.equal('userA', [getMyUserDoc.userB]),
-            ]);
+        const getDiscordUserDoc = await db.getDocument('db', 'discordUsers', userId);
 
-            if(listTheirDoc.documents.length > 0)
-            {
-                const getTheirUserDoc = listTheirDoc.documents[0];
-                const updateTheirDoc = await db.updateDocument('db', 'users', getTheirUserDoc.$id, { userA: getTheirUserDoc.userA, userB: getTheirUserDoc.userB, match: false }, [ Permission.read(Role.user(getTheirUserDoc.$id)) ]);
-            }
+        let done = false;
+
+        while(!done) 
+        {
+          const response = await databases.listDocuments('db', 'matches', [ Query.limit(25), Query.equal('userA', [getDiscordUserDoc.discordUsername], Query.equal('match', true) ]);
+          const documents = response.documents;
+    
+          for(const document of documents) {
+            await databases.deleteDocument('db', 'matches', document.$id);
+          }
+    
+          if(documents.length === 0) {
+            done = true;
+          }
         }
-      }
-      catch (err)
-      {
-          error('Error unmatching with user:', err);
+      } catch (err) {
+         error('Error deleting all matches documents:', err);
       }
 
       try {
         await db.deleteDocument('db', 'users', userId);
       } catch (err) {
         error('Error deleting user document:', err);
+      }
+
+      try {
+        await db.deleteDocument('db', 'lastMessageSent', userId);
+      } catch (err) {
+        error('Error deleting lastMessageSent document:', err);
       }
     
       try {
